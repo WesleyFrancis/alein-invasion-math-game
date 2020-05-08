481bed83aa55dceaa88df6ba10e5cefdf22511a1
Wesley Francis <59477200+WesleyFrancis@users.noreply.github.com>
2020-05-08 18:02:37 -0400

    random numbers

--- js/UIO/gameLevel.js
+++ js/UIO/gameLevel.js
@@ -5,9 +5,9 @@ const level={
     playingField:document.querySelector("#enime"),
 
 
-    addAlien()//build alien and add to screen
+    addAlien(num)//build alien and add to screen
     {
-        const enime=new Alien(200,6,2);
+        const enime=new Alien(200,num,2);
         this.playingField.appendChild(enime.spawnAlien());
     },
     startGame()//create emimies
@@ -16,42 +16,35 @@ const level={
         for(let i=0;i<GameInstance.maxEnime;i++)
         {
             fraction+="1fr ";
-            this.addAlien();
+            this.addAlien(getRandomInt(10));
         }
       //  console.log(fraction);
         
         this.playingField.style.gridTemplateColumns=fraction;
         
         this.moveEnimies();//make enimies move down
+        function getRandomInt(max) {
+            return Math.floor(Math.random() * Math.floor(max));
+          }
     },
     moveEnimies()
     {
       //  console.log(this.playingField.children[0]);
-        
+      console.log(window.innerHeight);
             let move=0;
             for(let i=0;i<this.playingField.children.length;i++)
             {
                 const timer=  setInterval(() => {
-                    move+=1;
+                    move+=.3;
                     const d=this.playingField.children[i].style.marginTop=`${move}px`;
-                
+                    
     
-                    if(move>=600)
+                    if(move>=window.innerHeight-320)//client y=spacecraft pix+cannonsize
                     {
                         clearInterval(timer);
                     }
                 }, 100);
             }
-        //   const timer=  setInterval(() => {
-        //         move+=5;
-        //         const d=this.playingField.children[2].style.marginTop=`${move}px`;
-            
-
-        //         if(move==600)
-        //         {
-        //             clearInterval(timer);
-        //         }
-        //     }, 100);
     }
 
 }
